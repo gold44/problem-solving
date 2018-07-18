529cddf842ee3e41a732523629ec25cdd3ead492
Patrick Lee <goldfourfour@gmail.com>
2018-07-18 23:49:13 +0800

    better solution, done

--- src/main/java/me/patrick/problems/CapturingStones.java
+++ src/main/java/me/patrick/problems/CapturingStones.java
@@ -9,6 +9,8 @@ public class CapturingStones extends Solution {
     private int gridNumber;
     private String givenInput;
     private int[][] boardGrid;
+    private static final int[] ROW = { -1, 1, 0, 0 };
+    private static final int[] COL = { 0, 0, -1, 1 };
 
 
     public CapturingStones(int gridNumber, String givenInput) {
@@ -21,21 +23,20 @@ public class CapturingStones extends Solution {
 
 
     private void setupTestData() {
-        String[] input = givenInput.split("\\n");
+        char[] inputs = givenInput.toCharArray();
 
-        // Go by ROW
-        for (int j = 0; j < input.length; j++) {
-            char[] dataX = input[j].toCharArray();
-            // For each ROW, go by COLUMN
-            for (int i = 0; i < dataX.length; i++) {
-                boardGrid[i][j] = Integer.valueOf(String.valueOf(dataX[i]));
+        System.out.println("Sample Data:");
+        for (int x = 0; x < gridNumber; x++) {
+            for (int y = 0; y < gridNumber; y++) {
+                boardGrid[x][y] = Integer.valueOf(String.valueOf(inputs[x * gridNumber + y]));
+                System.out.printf("%d ", boardGrid[x][y]);
             }
+            System.out.println();
         }
     }
 
 
-    @Override protected String execute() {
-        setupTestData();
+    private String solutionFirstAttempt() {
         Set<String> numberOfWhiteStones = new HashSet();
         System.out.println("Sample Data:");
 
@@ -70,5 +71,39 @@ public class CapturingStones extends Solution {
             System.out.println();
         }
         return String.valueOf(numberOfWhiteStones.size());
+    }
+
+
+    private String solutionBetter() {
+        int numberOfWhiteStones = 0;
+
+        for (int x = 0; x < gridNumber; x++) {
+            for (int y = 0; y < gridNumber; y++) {
+                if (boardGrid[x][y] == 1) {
+                    // Found the black stone, perform the check on its 4 surrounding
+                    for (int z = 0; z < 4; z++) {
+                        int r = x + ROW[z];
+                        int c = y + COL[z];
+
+                        if (r < 0 || r >= gridNumber || c < 0 || c >= gridNumber) {
+                            continue;
+                        }
+                        // if any of the 4 surrounding do not have black stone, count it.
+                        if (boardGrid[r][c] == 0) {
+                            // Do a marking on grid that has been occupy by white stone, so that it won't be counted again in future.
+                            boardGrid[r][c] = -1;
+                            numberOfWhiteStones++;
+                        }
+                    }
+                }
+            }
+        }
+        return String.valueOf(numberOfWhiteStones);
+    }
+
+
+    @Override protected String execute() {
+        setupTestData();
+        return solutionBetter();
     }
 }
